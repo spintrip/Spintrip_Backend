d57546d52722f1c04b972b7ce17a64cab77bb6f6
saswat555 <sapa19is@cmrit.ac.in>
2024-08-12 16:59:03 +0530

    user delete route

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -233,6 +233,25 @@ router.get('/users/:id', authenticate, async (req, res) => {
   }
 });
 
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    const additionalinfo = await UserAdditional.findByPk(req.params.id);
+    if (!additionalinfo) {
+      return res.status(404).json({ message: 'User additional not found' });
+    }
+    await additionalinfo.destroy();
+    await user.destroy();
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error deleting user', error });
+  }
+});
+
 router.get('/listings', authenticate, async (req, res) => {
   try {
     const listings = await Listing.findAll();
