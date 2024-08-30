2ecc8169972ade5328e8ca4bf25d10104bd51b91
saswat555 <sapa19is@cmrit.ac.in>
2024-08-30 13:52:56 +0530

    cashfree replacement with phone pe

--- Controller/paymentController.js
+++ Controller/paymentController.js
@@ -8,9 +8,9 @@ function roundToTwo(num) {
 }
 const initiatePayment = async (req, res) => {
   try {
+    const user = await User.findByPk(req.user.id);
     const { bookingId } = req.body;
     const booking = await Booking.findOne({ where: { Bookingid: bookingId } });
-
     if (!booking) {
       return res.status(404).json({ message: 'Booking not found' });
     }
@@ -20,63 +20,86 @@ const initiatePayment = async (req, res) => {
     }
     let amount;
     if ( booking.status == 2 ) {
-      // let transaction = await Transaction.findOne({ where: { Bookingid: bookingId, Transactionid: booking.Transactionid } });     
-      amount = roundToTwo( booking.totalUserAmount - transaction.totalAmount );
+      let transactions = await Transaction.findAll({ 
+        where: { Bookingid: bookingId },
+        attributes: ['totalAmount'] 
+      });
+    
+      let totalTransactionAmount = transactions.reduce((sum, transaction) => {
+        return sum + transaction.totalAmount;
+      }, 0);
+    
+      amount = roundToTwo( booking.totalUserAmount - totalTransactionAmount );
     }
     else {
       amount = roundToTwo( booking.totalUserAmount );
     }
-    const orderId = 'ORDER' + Date.now();
+    const orderId = uuid.v4();
+    await Transaction.create({
+      Transactionid: orderId,
+      Bookingid: bookingId,
+      id: req.user.id,
+      status: 1,
+      amount: booking.amount,
+      GSTamount: booking.GSTAmount,
+      totalAmount: booking.totalUserAmount
+    });
+    await Booking.update({ Transactionid: orderId }, { where: { Bookingid: bookingId } });
+    const payload = {
+        "merchantId": "M2207FVORVMF0",
+        "merchantTransactionId": orderId,
+        "merchantUserId": "M2207FVORVMF0",
+        "amount": amount * 100,
+        "redirectUrl": "https://spintrip.in/user/user-dashboard",
+        "redirectMode": "REDIRECT",
+        "callbackUrl": "https://spintripbackend.site/api/users/webhook/phonepe",
+        "mobileNumber": user.phone,
+        "paymentInstrument": {
+            "type": "PAY_PAGE"
+        }
+    };
 
-    const data = {
-      customer_details: {
-        customer_phone: '+918433745550',
-        customer_email: '',
-        customer_name: '',
-      },
-      link_notify: {
-        send_sms: false,
-        send_email: false,
-      },
-      link_amount: amount,
-      link_id: orderId,
-      link_currency: 'INR',
-      link_purpose: 'Booking Payment',
-    };
+    const saltKey = process.env.SALT_KEY; // Replace with your actual salt key
+    const saltIndex = 1; // Replace with the actual salt index
+
+    // Encode the payload to base64
+    const payloadBase64 = Buffer.from(JSON.stringify(payload)).toString('base64');
+
+    // Generate the string to hash for the request
+    const stringToHash = payloadBase64 + '/pg/v1/pay' + saltKey;
+    const sha256 = crypto.createHash('sha256').update(stringToHash).digest('hex');
+
+    // X-VERIFY header
+    const xVerify = `${sha256}###${saltIndex}`;
+
     const options = {
-      method: 'POST',
-      url: 'https://sandbox.cashfree.com/pg/links',
-      headers: {
-        'accept': 'application/json',
-        'content-type': 'application/json',
-        'x-api-version': '2023-08-01',
-        'x-client-id': process.env.CASHFREE_APP_ID,
-        'x-client-secret': process.env.CASHFREE_SECRET_KEY,
-      },
-      data: JSON.stringify(data),
-    };
-    const response = await axios.request(options);
-    if (response.data.link_status === 'ACTIVE') {
-      const paymentUrl = response.data.link_url;
-      console.log(paymentUrl);
-      await Booking.update({ Transactionid: orderId }, { where: { Bookingid: bookingId } });
-      await Transaction.create({
-          Transactionid: orderId,
-          Bookingid: bookingId,
-          id: req.user.id,
-          status: 1,
-          amount: booking.amount,
-          GSTamount: booking.GSTAmount,
-          totalAmount: booking.totalUserAmount
+        method: 'POST',
+        url: 'https://api.phonepe.com/apis/hermes/pg/v1/pay',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-VERIFY': xVerify
+        },
+        data: { "request": payloadBase64 }
+     };
+    
+     
+    axios
+        .request(options)
+        .then(function (response) {
+            console.log('Response received:', response.data);
+            if (response.data.success == true){
+            return res.status(200).json({ "paymentUrl": response.data.data.instrumentResponse.redirectInfo.url});
+            }
+            else{
+              return res.status(400).json( { message: 'Failed to create payment link', error: response.data.message });
+            }
+        })
+        .catch(function (error) {
+          return res.status(400).json( { message: 'Failed to create payment link' });
         });
-      return res.status(200).json({ paymentUrl });
-    } else {
-      return res.status(400).json({ message: 'Failed to create payment link', error: response.data.message });
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ message: 'Server error', error: error.message });
-  }
+} catch (error) {
+  return res.status(500).json( { message: 'Server Error' });
+}
 };
 
 
