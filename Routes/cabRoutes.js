6c9636dd9877a3d247d25a4588df8d2f46ca303d
saswat555 <sapa19is@cmrit.ac.in>
2024-11-27 14:32:55 +0530

    fixed keep-alive

--- Routes/cabRoutes.js
+++ Routes/cabRoutes.js
@@ -93,7 +93,7 @@ router.post('/driver/keep-alive', authenticate, async (req, res) => {
     }
 
     // Update the driver's location in the database
-    const vehicleId = CabToDriver.findOne({where: {driverid:driverId}});
+    const vehicleId = await CabToDriver.findOne({where: {driverid:driverId}});
     console.log(vehicleId)
     await VehicleAdditional.update(
       { latitude, longitude },
