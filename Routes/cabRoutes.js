45e7e7f4f7a439933f4666d0ae9cfa1ff440650e
saswat555 <sapa19is@cmrit.ac.in>
2024-11-28 14:04:45 +0530

    fixd send otp

--- Routes/cabRoutes.js
+++ Routes/cabRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { authenticate } = require("../Middleware/authMiddleware");
 const {
-  driverSignup,
   verifyDriverOtp,
   driverKeepAlive,
   addDriver,
