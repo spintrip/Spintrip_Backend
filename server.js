b236368c64072c6b8ea3560091a2b7aa0c4317bd
Tiasha <pratyay.mazumdar02@gmail.com>
2026-03-27 14:44:15 +0530

    Spintrip Version 8

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.use(cors({
     ],
 }));
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false,  limit: '50mb'  }));
 app.use(cookieParser());
 app.use(compression());
 app.use((req, res, next) => {
